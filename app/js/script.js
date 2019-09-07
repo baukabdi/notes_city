9816ab897c65ed87f2e14da099803c00d25021bc
baukabdi <bauka.abdi@gmail.com>
2019-09-08 00:06:39 +0600

    add 2 block features

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+	// Landing owl carousel START
 	$(function() {
 		var owl = $('.features__landing .carousel__wrap');
 		owl.owlCarousel({
@@ -12,16 +13,43 @@ $(document).ready(function() {
 		});
 
 		// next carousel
-    $('.carousel-arrow__right').click(function(e) {
+    $('.features__landing .carousel-arrow__right').click(function(e) {
       e.preventDefault();
       owl.trigger('next.owl.carousel');
     })
 
     // prev carousel
-    $('.carousel-arrow__left').click(function(e) {
+    $('.features__landing .carousel-arrow__left').click(function(e) {
       e.preventDefault();
       owl.trigger('prev.owl.carousel', [300]);
     })
 	})
+	// Landing owl carousel END
+
+	// Online magazin owl carousel START
+	$(function() {
+		var owl = $('.features-content__online-magazine .carousel__wrap');
+		owl.owlCarousel({
+			items: 1,
+			dots: true,
+			dotsSpeed: 300,
+			nav: false,
+			smartSpeed: 1000,
+			dotsContainer: '#carousel-dots-magazine',
+		});
+
+		// next carousel
+    $('.features-content__online-magazine .carousel-arrow__right').click(function(e) {
+      e.preventDefault();
+      owl.trigger('next.owl.carousel');
+    })
+
+    // prev carousel
+    $('.features-content__online-magazine .carousel-arrow__left').click(function(e) {
+      e.preventDefault();
+      owl.trigger('prev.owl.carousel', [300]);
+    })
+	});
+	// Online magazin owl carousel END
 	
 });
